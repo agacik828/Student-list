1d539191524c6c6cf1bfa2162c19c6f7b55e1bed
Your agacik828 <agacik828@wp.plgit config --global user.name Your>
2021-10-28 17:55:02 +0200

    Initial commit

---
+++ src/main/java/agata/twardowska/Student/list/StudentListApplication.java
@@ -0,0 +1,13 @@
+package agata.twardowska.Student.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agata/twardowska/Student/list/StudentListApplicationTests.java
@@ -0,0 +1,13 @@
+package agata.twardowska.Student.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
